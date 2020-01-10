efc9e515ad04b2405d25c374472d2fc8bd928c54
Jeffrey Ilar <jilar@petzent.com>
2020-01-10 15:21:02 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+//import express library
+const express = require('express');
+//import mongoose libary to connect to mongodb
+const mongoose =require('mongoose');
+
+//create app object, will represent entire application
+const app = express();
+
+//connect to database
+const mongoUri='mongodb+srv://admin:Password1!@cluster0-r8wji.mongodb.net/test?retryWrites=true&w=majority';
+
+//connect- objects in function prevent common error messages
+mongoose.connect(mongoUri,{
+  useNewUrlParser: true,
+  useCreateIndex:true,
+  useUnifiedTopology:true
+});
+
+//each succesful connect cause this callback to run
+mongoose.connection.on('connected', () =>{
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) =>{
+  console.log('Error connecting: ', err);
+});
+
+//set up route hander
+//anytime someone makes a get type http request to route of application, do this function
+app.get('/',(req, res) =>{
+  res.send("Hi There ");
+});
+
+//have app listen to particular port on local machine
+app.listen(3000,()=>{
+  console.log('Listening on port 3000');
+})
